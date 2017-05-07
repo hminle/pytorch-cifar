490449472106aaa9c73c107d4049333eb7fdb821
Hoang Le <lemin1991@postech.ac.kr>
2017-05-07 13:45:11 +0900

    add lr scheduler

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from torch.autograd import Variable
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--epochs', '-e', default=200, type=int, help='num of epochs')
 args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available()
@@ -36,10 +37,10 @@ transform = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -56,10 +57,10 @@ if args.resume:
 else:
     print('==> Building model..')
     # net = VGG('VGG19')
-    # net = ResNet18()
+    net = ResNet18()
     # net = GoogLeNet()
-    # net = DenseNet121()
-    net = ResNeXt29_2x64d()
+    #net = DenseNet121()
+    #net = ResNeXt29_2x64d()
 
 if use_cuda:
     net.cuda()
@@ -69,8 +70,21 @@ if use_cuda:
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
+# Schedule LR
+def exp_lr_scheduler(optimizer, epoch, init_lr=args.lr, lr_decay_epoch=10):
+    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
+
+    if epoch % lr_decay_epoch == 0:
+        print('LR is set to {}'.format(lr))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    return optimizer
+
 # Training
-def train(epoch):
+def train(epoch, optimizer):
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
@@ -130,6 +144,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
+for epoch in range(start_epoch, start_epoch+args.epochs):
+    optimizer = exp_lr_scheduler(optimizer, epoch, lr_decay_epoch=50)	
+    train(epoch, optimizer)
     test(epoch)
